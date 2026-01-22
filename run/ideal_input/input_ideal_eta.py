31d9b430cd0045790660be0398e9497ec53bcd4c
wuhy <wuhy777@qq.com>
2026-01-22 17:37:04 +0800

    20260122-input

--- run/ideal_input/input_ideal_eta.py
+++ run/ideal_input/input_ideal_eta.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+from pathlib import Path
 
 def write_input_terrain(ds):
 	
@@ -27,19 +28,34 @@ def write_input_terrain(ds):
 	print("input_terrain file has been created.")
 
 def write_input_sounding(ds):
-	
-	psfc = ds['psfc_low'].values / 100
-	t2 = ds['t2_low'].values
-	q2 = ds['q2_low'].values * 1000
-	theta2 = t2 * (1000 / psfc) ** 0.286
+
+	rd = 287.04
+	g = 9.81
+	cp = 1004.6
+	cv = 717.6
 
 	u = ds['ua_low'].values
 	v = ds['va_low'].values
 	t = ds['t_low'].values
-	p = ds['pressure'].values / 100
-	z = ds['geopt_low'].values / 9.81
-	q = ds['q_low'].values * 1000
+	p = ds['p_low'].values / 100 # convert to hPa
+	z = ds['geopt_low'].values / g
+	q = ds['q_low'].values * 1000 # convert to g/kg
 	theta = t * (1000 / p) ** 0.286
+
+	z0 = z[0]
+	p0 = p[0] * 100 # convert back to Pa
+	theta0 = theta[0]
+	q0 = q[0] / 1000 # convert back to kg/kg
+
+	rho0 = 100000 / (rd * theta0) * (p0 / 100000)**(cv/cp)
+
+	dp = - g * rho0 * z0 * (1 + q0)
+	psfc = p0 - dp
+	
+	q2 = q0 * 1000
+	theta2 = 100000 / (rd * rho0) * (psfc / 100000)**(cv/cp)
+
+	psfc = psfc / 100
 
 	with open('input_sounding', 'w') as file:
 		# Write surface data
@@ -60,12 +76,19 @@ def write_input_sounding(ds):
 
 def main():
 
-	timeidx = 42
-	south_north_idx = 17
-	west_east_idx = 13
-	ds = xr.open_dataset('/nfs/raid66/hywu/GWNN/data/04_gwnn_out/local/ds_train_2007_interp.nc').isel(time=timeidx, south_north=south_north_idx, west_east=west_east_idx)
+	pathdir = Path('/nfs/raid66/hywu/GWNN/experiments/gwnn/20260117_local_phys/data')
+	paths = sorted(list(pathdir.glob('GWNNout*00.nc')))
+	paths_train_1 = paths[1344:2208] # 2007-12-01 to 2007-12-09
+	paths_train_2 = paths[2304:3168] # 2007-12-11 to 2007-12-19
+	paths_train_3 = paths[3264:4128] # 2007-12-21 to 2007-12-29
+	paths_train = paths_train_1 + paths_train_2 + paths_train_3 # train set (2007-12 except 2007-12-10 2007-12-20 2007-12-30)
 	
-	ds = ds.dropna(dim='pressure')
+	timeidx = 320
+	south_north_idx = 10
+	west_east_idx = 13
+	path = paths_train[timeidx]
+	ds = xr.open_dataset(path).isel(time=0, south_north=south_north_idx, west_east=west_east_idx)
+	
 	write_input_terrain(ds)
 	write_input_sounding(ds)
 	
